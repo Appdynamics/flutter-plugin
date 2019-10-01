d64d99435ee3a661e2ab9c2fd186c6ded18e13ca
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-10-01 12:29:47 +0200

    Add ios code to example app

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ This project is a starting point for a Flutter
 a specialized package that includes platform-specific implementation code for
 Android and/or iOS.
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 
 ### Config files
@@ -34,4 +34,5 @@ APPDYNAMICS_EUM_KEY=**-***-**-***
 ```
 
 #### IOS Configuration
-TBD
+
+Run pod install
--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,13 +1,23 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <ADEUMInstrumentation/ADEumInstrumentation.h>
 
 @implementation AppDelegate
 
+- (void)initAppDynamicsAgent {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"AppDynamics" ofType:@"plist"];
+    NSDictionary *settings = [[NSDictionary alloc] initWithContentsOfFile:path];
+    ADEumAgentConfiguration *config = [[ADEumAgentConfiguration alloc] initWithAppKey: [settings valueForKey:@"AppKey"]];
+    config.loggingLevel = ADEumLoggingLevelAll;
+    [ADEumInstrumentation initWithConfiguration:config];
+    // Override point for customization after application launch.
+}
+
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    [self initAppDynamicsAgent];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
