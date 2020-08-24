a19216acfd7b7105d85e152d7fb636fb0e8c3412
Michael Sickles <michael.sickles@appdynamics>
2020-08-24 16:26:38 -0400

    Pluign now implements the FlutterPlugin class as well

--- android/src/main/java/com/appdynamics/appdynamics_mobilesdk/AppdynamicsMobilesdkPlugin.java
+++ android/src/main/java/com/appdynamics/appdynamics_mobilesdk/AppdynamicsMobilesdkPlugin.java
@@ -17,6 +17,9 @@ import java.util.UUID;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -26,16 +29,43 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import android.util.Log;
 
 /** AppdynamicsMobilesdkPlugin */
-public class AppdynamicsMobilesdkPlugin implements MethodCallHandler {
+public class AppdynamicsMobilesdkPlugin implements FlutterPlugin, MethodCallHandler {
   private static Map<String, HttpRequestTracker> trackers = new HashMap<String, HttpRequestTracker>();
   private static Map<String, SessionFrame> sessionFrames = new HashMap<String, SessionFrame>();
+  private MethodChannel channel;
+  private static final String channelName = "appdynamics_mobilesdk";
 
   private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
+  private static void setup(AppdynamicsMobilesdkPlugin plugin, BinaryMessenger binaryMessenger) {
+    plugin.channel = new MethodChannel(binaryMessenger, channelName);
+    plugin.channel.setMethodCallHandler(plugin);
+
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    setup(this, flutterPluginBinding.getBinaryMessenger());
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+
   /** Plugin registration. */
+  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+  // plugin registration via this function while apps migrate to use the new Android APIs
+  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+  //
+  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+  // in the same class.
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "appdynamics_mobilesdk");
-    channel.setMethodCallHandler(new AppdynamicsMobilesdkPlugin());
+    AppdynamicsMobilesdkPlugin plugin = new AppdynamicsMobilesdkPlugin();
+    setup(plugin, registrar.messenger());
   }
 
   public String startRequest(URL url) {
